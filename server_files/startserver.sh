08346badcdac90072971f01f21e018076634ab82
Jim Ramsay <i.am@jimramsay.com>
2022-03-28 22:34:03 -0400

    startserver.sh: Build starter jar and URL from a version variable
    This should make future version-bumps of the jar even easier.
    
    Signed-off-by: Jim Ramsay <i.am@jimramsay.com>

--- server_files/startserver.sh
+++ server_files/startserver.sh
@@ -8,8 +8,9 @@ if grep -q 'ramDisk:.*yes' server-setup-config.yaml; then
     DO_RAMDISK=1
 fi
 
-STARTER_JAR="serverstarter-2.1.1.jar"
-URL="https://github.com/TeamAOF/ServerStarter/releases/download/v2.1.1/$STARTER_JAR"
+STARTER_VERSION="2.1.1"
+STARTER_JAR="serverstarter-$STARTER_VERSION.jar"
+URL="https://github.com/TeamAOF/ServerStarter/releases/download/v$STARTER_VERSION/$STARTER_JAR"
 if [ -f $STARTER_JAR ]; then
     echo "Skipping download. Using existing $STARTER_JAR"
 else
